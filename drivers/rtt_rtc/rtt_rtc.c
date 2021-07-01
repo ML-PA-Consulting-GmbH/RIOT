b6289a21c0eef6b0c99b144e7022f83f5e3354c3
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2021-07-01 19:53:30 +0200

    fixup! rtt_rtc: fix rtc_settimeofday()

--- drivers/rtt_rtc/rtt_rtc.c
+++ drivers/rtt_rtc/rtt_rtc.c
@@ -210,7 +210,7 @@ int rtc_settimeofday(uint32_t s, uint32_t us)
 	/* disable alarm to prevent race condition */
 	rtt_clear_alarm();
 	uint32_t now = ((uint64_t)us * RTT_SECOND) / US_PER_SEC;
-	rtc_now      = s - SECONDS(now);
+	rtc_now      = s;
 	rtt_set_counter(now);
 	/* calculate next wake-up period */
 	_update_alarm(0);
