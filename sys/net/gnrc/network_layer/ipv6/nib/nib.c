c00033b7cb1e7c08461ceca5886147ab93c7720e
Benjamin Valentin <benpicco@beuth-hochschule.de>
2021-08-03 18:23:17 +0200

    gnrc_ipv6_nib: add hook for Prefix Information Option

--- sys/net/gnrc/network_layer/ipv6/nib/nib.c
+++ sys/net/gnrc/network_layer/ipv6/nib/nib.c
@@ -86,6 +86,14 @@ static void _handle_rdnss_timeout(sock_udp_ep_t *dns_server);
 #endif
 /** @} */
 
+/* Callback to hook into Prefix Information Option */
+__attribute__ ((weak))
+void gnrc_nib_rtr_adv_pio_cb(gnrc_netif_t *netif, const ndp_opt_pi_t *pio)
+{
+    (void) netif;
+    (void) pio;
+}
+
 void gnrc_ipv6_nib_init(void)
 {
     evtimer_event_t *tmp;
@@ -440,6 +448,11 @@ void gnrc_ipv6_nib_change_rtr_adv_iface(gnrc_netif_t *netif, bool enable)
                      next_rs_time);
     }
     gnrc_netif_release(netif);
+}
+
+void gnrc_ipv6_nib_send_final_rtr_adv(gnrc_netif_t *netif)
+{
+    _snd_rtr_advs(netif, &ipv6_addr_all_nodes_link_local, true);
 }
 #endif  /* CONFIG_GNRC_IPV6_NIB_ROUTER */
 
@@ -817,7 +830,7 @@ static void _handle_rtr_adv(gnrc_netif_t *netif, const ipv6_hdr_t *ipv6,
                 next_timeout = _min(next_timeout, min_pfx_timeout);
 
                 /* automatically configure subnets on downstream interfaces */
-                _configure_subnets(netif, (ndp_opt_pi_t *)opt);
+                gnrc_nib_rtr_adv_pio_cb(netif, (ndp_opt_pi_t *)opt);
                 break;
             }
             /* ABRO was already secured in the option check above */
