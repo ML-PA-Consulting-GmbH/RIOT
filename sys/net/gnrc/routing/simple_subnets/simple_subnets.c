c4cea8ae4aea4a20ceb5bfb41eb41d2f4270ea33
Benjamin Valentin <benpicco@beuth-hochschule.de>
2021-08-04 12:34:29 +0200

    fixup! fixup! fixup! fixup! gnrc_ipv6_nib: auto-configure downstream subnets

--- sys/net/gnrc/routing/simple_subnets/simple_subnets.c
+++ sys/net/gnrc/routing/simple_subnets/simple_subnets.c
@@ -51,15 +51,17 @@
  */
 
 #include "net/gnrc/ipv6/nib.h"
+#include "net/gnrc/ndp.h"
 
 #define ENABLE_DEBUG 0
 #include "debug.h"
 
 static char addr_str[IPV6_ADDR_MAX_STR_LEN];
 
-void gnrc_nib_rtr_adv_pio_cb(gnrc_netif_t *upstream, const ndp_opt_pi_t *pio)
+void gnrc_ipv6_nib_rtr_adv_pio_cb(gnrc_netif_t *upstream, const ndp_opt_pi_t *pio)
 {
     gnrc_netif_t *downstream = NULL;
+    gnrc_pktsnip_t *ext_opts = NULL;
     const ipv6_addr_t *prefix = &pio->prefix;
     uint32_t valid_ltime = byteorder_ntohl(pio->valid_ltime);
     uint32_t pref_ltime = byteorder_ntohl(pio->pref_ltime);
@@ -77,7 +79,7 @@ void gnrc_nib_rtr_adv_pio_cb(gnrc_netif_t *upstream, const ndp_opt_pi_t *pio)
     new_prefix_len = prefix_len + 32 - __builtin_clz(subnets);
 
     if (new_prefix_len > 64) {
-        DEBUG("nib: can't split /%u into %u subnets\n", prefix_len, subnets);
+        DEBUG("simple_subnets: can't split /%u into %u subnets\n", prefix_len, subnets);
         return;
     }
 
@@ -93,7 +95,7 @@ void gnrc_nib_rtr_adv_pio_cb(gnrc_netif_t *upstream, const ndp_opt_pi_t *pio)
         new_prefix.u64[0].u64 |= (uint64_t)subnets-- << (63 - prefix_len);
         new_prefix.u64[0] = byteorder_htonll(new_prefix.u64[0].u64);
 
-        DEBUG("nib: configure prefix %s/%u on %u\n",
+        DEBUG("simple_subnets: configure prefix %s/%u on %u\n",
               ipv6_addr_to_str(addr_str, &new_prefix, sizeof(addr_str)),
               new_prefix_len, downstream->pid);
 
@@ -102,14 +104,16 @@ void gnrc_nib_rtr_adv_pio_cb(gnrc_netif_t *upstream, const ndp_opt_pi_t *pio)
 
         /* start advertising subnet */
         gnrc_ipv6_nib_change_rtr_adv_iface(downstream, true);
+
+        /* add route information option with new subnet */
+        ext_opts = gnrc_ndp_opt_ri_build(&new_prefix, new_prefix_len, valid_ltime,
+                                         NDP_OPT_RI_FLAGS_PRF_NONE, ext_opts);
+        if (ext_opts == NULL) {
+            DEBUG("simple_subnets: No space left in packet buffer. Not adding RIO\n");
+        }
     }
 
-    /* Disable router advertisements on upstream interface.
-     * 1. Does not confuse the upstream router to add the border router to its
-     *    default router list and
-     * 2. Includes Route Information Option in last router advertisement to
-     *    inform upstream router about downstream subnets.
-     */
-    gnrc_ipv6_nib_send_final_rtr_adv(upstream);
+    /* immediately send an RA with RIO */
+    gnrc_ndp_rtr_adv_send(upstream, NULL, &ipv6_addr_all_nodes_link_local, true, ext_opts);
 }
 /** @} */
