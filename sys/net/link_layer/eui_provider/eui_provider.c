ba50f8f3697d01d64c4b6a700c95db635411421b
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2022-06-23 14:25:35 +0200

    net/eui_provider: provide netif index to EUI provder function
    An EUI provider can provide EUIs for multiple interfaces based on
    their index.
    
    For this is should get the index of the interface, not the index of
    the EUI provider.

--- sys/net/link_layer/eui_provider/eui_provider.c
+++ sys/net/link_layer/eui_provider/eui_provider.c
@@ -41,7 +41,7 @@ void netdev_eui48_get(netdev_t *netdev, eui48_t *addr)
 #else
         (void) netdev;
 #endif
-        if (eui48_conf[i].provider(i, addr) == 0) {
+        if (eui48_conf[i].provider(netdev->index, addr) == 0) {
             return;
         }
     }
@@ -71,7 +71,7 @@ void netdev_eui64_get(netdev_t *netdev, eui64_t *addr)
 #else
         (void) netdev;
 #endif
-        if (eui64_conf[i].provider(i, addr) == 0) {
+        if (eui64_conf[i].provider(netdev->index, addr) == 0) {
             return;
         }
     }
