181a53bfde138fba785d2768402fccbef6acf25e
Daniel Lockau <daniel.lockau@ml-pa.com>
2025-02-11 17:41:10 +0100

    fixup! wip

--- dist/tools/sbom/src/riot-sbom/build_scanner.py
+++ dist/tools/sbom/src/riot-sbom/build_scanner.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import json
+import logging
+import re
 import subprocess
-import argparse
+import tempfile
 from pathlib import Path
 
 class BuildScanner(object):
@@ -15,7 +16,18 @@ class BuildScanner(object):
         self.file_data = []
 
     def run(self):
-        pass
+        pkg_info = BuildScanner._run_pkg_info(self.app_dir)
+        self.app_name = pkg_info['application']
+        with tempfile.TemporaryDirectory() as tempdir:
+            trace_file = os.path.join(tempdir, 'trace.log')
+            BuildScanner._run_traced_build(self.app_dir,
+                                           trace_file)
+            file_paths = BuildScanner._parse_trace_file(trace_file)
+        self.package_data = BuildScanner._complete_package_data(pkg_info['packages'],
+                                                                file_paths)
+        self.file_data = BuildScanner._build_file_data(file_paths,
+                                                       self.app_dir,
+                                                       self.package_data)
 
     def get_app_name(self):
         if not self.app_name:
@@ -31,3 +43,93 @@ class BuildScanner(object):
         if not self.file_data:
             raise RuntimeError('Scanner must be run before retrieving data')
         return self.file_data
+
+    @staticmethod
+    def _run_pkg_info(app_dir):
+        logging.info('Retrieving package information for build')
+        pkg_info_run = subprocess.run(['make', 'info-sbom-input'],
+                                      cwd=app_dir,
+                                      stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+        if pkg_info_run.returncode != 0:
+            raise RuntimeError('Failed to run make info-sbom')
+        output = pkg_info_run.stdout.decode('utf-8')
+        start_marker = ">>>> BEGIN SBOM INPUT"
+        end_marker = "<<<< END SBOM INPUT"
+        start_idx = output.find(start_marker)
+        end_idx = output.find(end_marker)
+        if start_idx == -1 or end_idx == -1:
+            raise RuntimeError('Failed to find SBOM input markers')
+        sbom_input = output[start_idx + len(start_marker):end_idx]
+        pkg_info = json.loads(sbom_input)
+        return pkg_info
+
+    @staticmethod
+    def _run_traced_build(app_dir: str, trace_file: str):
+        logging.info('Retrieving file information for build (this may take a while)')
+        build_cmd = ["strace", "-f",
+                    "-e", "trace=openat,open",
+                    "-e", "quiet=attach,exit,path-resolution,personality,thread-execve,superseded",
+                    "-o", f"{trace_file}", "make", "-j"]
+        trace_run = subprocess.run(build_cmd,
+                                   cwd=app_dir,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        if trace_run.returncode != 0:
+            raise RuntimeError(f'Failed to run make trace-build (stderr="{trace_run.stderr.decode("utf-8")}")')
+
+    @staticmethod
+    def _parse_trace_file(trace_file: str):
+        logging.info('Parsing trace file')
+        file_paths = set()
+        path_matcher = re.compile(r'[^"]*"(.*)"[^"]*')
+        file_matcher = re.compile(r'.*\.(h|hpp|c|cpp|hxx|cxx|c\+\+|s|asm)$')
+        with open(trace_file, 'r') as f:
+            for line in f:
+                if 'openat(' in line or "open(" in line:
+                    match = path_matcher.match(line)
+                    if match:
+                        path = match.group(1)
+                        if file_matcher.match(path):
+                            file_paths.add(path)
+        return sorted(file_paths)
+
+    @staticmethod
+    def _build_file_data(file_paths: list, app_dir: str, package_data: list):
+        logging.info('Building file data')
+        file_data = []
+        for file_path in file_paths:
+            file_info = BuildScanner._get_file_info(file_path, app_dir, package_data)
+            file_data.append(file_info)
+        return file_data
+
+    @staticmethod
+    def _complete_package_data(pkg_data: list, file_paths: list):
+        logging.info('Completing package data')
+        package_data = pkg_data
+        for file in file_paths:
+            for package in package_data:
+                if file.startswith(package['path']):
+                    break
+                else:
+                    package_data.append({
+                        'name': 'unknown',
+                        'basedir': 'unknown',
+                        'url': None,
+                        'version': None,
+                        'license': 'unknown'
+                    })
+        return package_data
+
+    @staticmethod
+    def _get_file_info(file_path: str, app_dir: str, package_data: list):
+        file_info = {
+            'path': file_path,
+            'package': None,
+            'license': None
+        }
+        for package in package_data:
+            if file_path.startswith(package['path']):
+                file_info['package'] = package['name']
+                break
+        return file_info
--- makefiles/info.inc.mk
+++ makefiles/info.inc.mk
@@ -258,22 +258,43 @@ info-rust:
 	@echo "You can also call cargo related commands with \`make cargo-command CARGO_COMMAND=\"cargo check\"\`."
 	@echo "Beware that the way command line arguments are passed in is not consistent across cargo commands, so adding \`--profile $(CARGO_PROFILE)\` or other flags from above as part of CARGO_COMMAND may be necessary."
 
-info-sbom:
+info-sbom-input:
 	@echo ">>>> BEGIN SBOM INPUT"
 	@echo "{"
 	@echo "  \"application\": \"$(APPLICATION)\","
-	@echo "  \"packages\": ["
+	@echo "  \"application_dir\": \"$(CURDIR)\","
+	@echo "  \"external_modules\": ["
+	@imod=0; for module_path in $(EXTERNAL_MODULE_PATHS); \
+	do \
+	  imod=$$(( $${imod} + 1 )); \
+	  [ $${imod} -gt 1 ] && echo -n ","; \
+	  echo "{\"name\": \"$$(basename $${module_path})\", \"srcdir\": \"$${module_path}\"}"; \
+	done
+	@echo "  ],"
 	@riot_version=$$(git rev-parse --verify HEAD); \
 	  riot_remote_name=$$(git branch -r --color=never --no-column --no-format --contains $${riot_version} | head -n1 | cut -d / -f1 | sed -E 's/^\s*//'); \
 	  riot_or_fork_url=$$(git remote get-url $${riot_remote_name}); \
-	  echo "{\"name\": \"RIOT OS\", \"url\": \"$${riot_or_fork_url}\", \"version\": \"$${riot_version}\", \"license\": \"LGPL-2.1\"}"
-	@for pkg in $(sort $(USEPKG)); \
+	  echo "\"riot\": {\"basedir\": \"$(RIOTBASE)\", \"url\": \"$${riot_or_fork_url}\", \"version\": \"$${riot_version}\", \"license\": \"LGPL-2.1\"},"
+	@echo "  \"packages\": ["
+	@ipkg=0; for pkg in $(sort $(USEPKG)); \
 	  do \
-	    pkg_vars=$$(make -j1 -p -n -C "$(RIOTBASE)/pkg/$${pkg}" 2>/dev/null | grep -E '(PKG_URL|PKG_VERSION|PKG_LICENSE)\s*:?='); \
-	    pkg_url=$$(echo $${pkg_vars} | grep -E '^PKG_URL' "$(RIOTBASE)/pkg/$${pkg}/Makefile" | sed -E 's/^\s*PKG_URL\s*:?=\s*//'); \
-	    pkg_ver=$$(echo $${pkg_vars} | grep -E '^PKG_VERSION' "$(RIOTBASE)/pkg/$${pkg}/Makefile" | sed -E 's/^\s*PKG_VERSION\s*:?=\s*//'); \
-	    pkg_lic=$$(echo $${pkg_vars} | grep -E '^PKG_LICENSE' "$(RIOTBASE)/pkg/$${pkg}/Makefile" | sed -E 's/^\s*PKG_LICENSE\s*:?=\s*//'); \
-	    echo " ,{\"name\": \"$${pkg}\", \"url\": \"$${pkg_url}\", \"version\": \"$${pkg_ver}\", \"license\": \"$${pkg_lic}\"}"; \
+	    ipkg=$$(( $${ipkg} + 1 )); \
+	    [ $${ipkg} -gt 1 ] && echo -n ","; \
+	    for pth in $(PKG_PATHS); \
+		do \
+		  case "$${pth}" in \
+		    */$${pkg}/) \
+			  pkg_def_dir="$${pth}"; \
+			  break; \
+			  ;; \
+		  esac; \
+		done; \
+		pkg_source_dir="$(RIOTBASE)/build/pkg/$${pkg}"; \
+	    pkg_vars=$$(make -j1 -p -n -C "$${pkg_def_dir}" 2>/dev/null | grep -E '(PKG_URL|PKG_VERSION|PKG_LICENSE)\s*:?='); \
+	    pkg_url=$$(echo $${pkg_vars} | grep -E '^PKG_URL' "$${pkg_def_dir}/Makefile" | sed -E 's/^\s*PKG_URL\s*:?=\s*//'); \
+	    pkg_ver=$$(echo $${pkg_vars} | grep -E '^PKG_VERSION' "$${pkg_def_dir}/Makefile" | sed -E 's/^\s*PKG_VERSION\s*:?=\s*//'); \
+	    pkg_lic=$$(echo $${pkg_vars} | grep -E '^PKG_LICENSE' "$${pkg_def_dir}/Makefile" | sed -E 's/^\s*PKG_LICENSE\s*:?=\s*//'); \
+	    echo "{\"name\": \"$${pkg}\", \"definition_dir\": \"$${pkg_def_dir}\", \"source_dir\": \"$${pkg_source_dir}\", \"url\": \"$${pkg_url}\", \"version\": \"$${pkg_ver}\", \"license\": \"$${pkg_lic}\"}"; \
 	  done
 	@echo "]}"
 	@echo "<<<< END SBOM INPUT"
