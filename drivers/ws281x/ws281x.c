8604202417a777c6a9ff09794b6a1b11aee73775
Fabian Hüßler <fabian.huessler@ml-pa.com>
2026-02-03 10:24:24 +0100

    drivers/ws281x: move default implementations to c file

--- drivers/ws281x/ws281x.c
+++ drivers/ws281x/ws281x.c
@@ -24,6 +24,7 @@
 #include <string.h>
 
 #include "ws281x.h"
+#include "ws281x_backend.h"
 #include "ws281x_constants.h"
 #include "ws281x_params.h"
 #include "periph/gpio.h"
@@ -41,3 +42,27 @@ void ws281x_set_buffer(void *_dest, uint16_t n, ws281x_pixel_t c)
     dest[WS281X_BYTES_PER_DEVICE * n + WS281X_OFFSET_W] = c.w;
 #endif
 }
+
+/* Backend may implement these functions */
+
+#if !defined(WS281X_HAVE_INIT)
+int ws281x_init(ws281x_t *dev, const ws281x_params_t *params) {
+    dev->params = *params;
+    return 0;
+}
+#endif
+
+#if !defined(WS281X_HAVE_PREPARE_TRANSMISSION)
+void ws281x_prepare_transmission(ws281x_t *dev)
+{
+    (void)dev;
+}
+#endif
+
+#if !defined(WS281X_HAVE_END_TRANSMISSION)
+void ws281x_end_transmission(ws281x_t *dev)
+{
+    (void)dev;
+    xtimer_usleep(WS281X_T_END_US);
+}
+#endif
